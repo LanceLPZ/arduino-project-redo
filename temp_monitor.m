f632c2f809c4da386df6f2d3912994cfbee4459c
Lance Lopez <lancepatricklopez20@gmail.com>
2025-05-07 23:52:39 +0100

    TASK NUMBER DOS

---
+++ temp_monitor.m
@@ -0,0 +1,70 @@
+function temp_monitor(a)
+
+therm = 'A0';
+red = 'D2';
+yellow = 'D3';
+green = 'D4';
+
+voltagezero = 0.5;       
+temperaturecoefficient = 0.01; 
+
+results = [];
+timegraph = [];
+index = 1;
+redblink = 0;
+yellowblink = 0;
+
+figure;
+graph = animatedline('Color','b','LineWidth',2);
+xlabel('Time (s)');
+ylabel('Temperature (°C)');
+title('Live Temperature Plot');
+grid on;
+startTime = tic;
+
+voltage = readVoltage(a,therm);
+temperature = (voltage-voltagezero)/temperaturecoefficient;
+results(index) = temperature;
+timegraph(index) = 0; % Time = 0 at start
+addpoints(graph, 0, temperature);
+drawnow;
+
+while true
+    voltage = readVoltage(a, therm);
+    temperature = (voltage - voltagezero) / temperaturecoefficient;
+    currentTime = toc(startTime);
+    results(index) = temperature;
+    timegraph(index) = currentTime;
+    index = index + 1;
+
+    addpoints(graph, currentTime, temperature);
+    drawnow limitrate;
+
+    writeDigitalPin(a, red, 0)
+    writeDigitalPin(a, yellow, 0)
+    writeDigitalPin(a, green,0)
+
+if temperature >= 18 && temperature <= 24
+
+    writeDigitalPin(a, red, 0);
+    writeDigitalPin(a, yellow, 0);
+    writeDigitalPin(a, green, 1);
+    
+elseif temperature > 24
+   
+    redblink = ~redblink;
+    writeDigitalPin(a, red, redblink);
+
+elseif temperature < 18
+
+    yellowblink = ~yellowblink;
+    writeDigitalPin(a, yellow, yellowblink);
+end
+
+pause(0.5);
+
+
+end
+
+
+end
