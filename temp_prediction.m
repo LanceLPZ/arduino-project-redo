64f41f25d33cc254d95661222ec6792acda83485
Lance Lopez <lancepatricklopez20@gmail.com>
2025-05-08 22:36:13 +0100

    started temp prediction

--- temp_prediction.m
+++ temp_prediction.m
@@ -1,1 +1,24 @@
 function temp_prediction(a)
+
+therm = 'A0'
+red = 'D4'
+yellow = 'D3'
+green = 'D2'
+
+
+duration = 180
+voltagezero = 0.5
+temperaturecoefficient = 0.01
+datapoints = 10 
+
+voltagearray = zeros(1, duration)
+temparray = zeros(1,duration)
+timearray = zeros(1, duration)
+
+for time = 1:duration
+voltage = readVoltage(a,therm);
+temperature = (voltage-voltagezero)/temperaturecoefficient;
+
+
+
+
